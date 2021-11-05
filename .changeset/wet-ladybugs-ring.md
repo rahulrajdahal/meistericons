e3131de049b0aa21ad10fe9798353634f3182a37
Markaa10 <rahulrajdahal@gmail.com>
2021-11-05 18:44:16 +0545

    widthchanges

---
+++ .changeset/wet-ladybugs-ring.md
@@ -0,0 +1,5 @@
+---
+"meistericons": patch
+---
+
+iconswidths change
--- scripts/build.js
+++ scripts/build.js
@@ -14,10 +14,6 @@ async function transformSVGtoJSX(file, componentName, format) {
     {
       icon: false,
       replaceAttrValues: { "#00497A": "{props.color || '#00497A'}" },
-      svgProps: {
-        width: 24,
-        height: 24,
-      },
     },
     { componentName }
   );
