d9e29ea05d35c9c9742f331716428c614a8264d7
rahulrajdahal <rahulrajdahal@gmail.com>
2023-11-13 01:08:16 +0545

    update

--- packages/react/rollup.config.mjs
+++ packages/react/rollup.config.mjs
@@ -1,25 +1,37 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import sourcemaps from "rollup-plugin-sourcemaps";
 import esbuild, { minify } from "rollup-plugin-esbuild";
 import pkg from "./package.json" assert { type: "json" };
+import terser from "@rollup/plugin-terser";
+import { visualizer } from "rollup-plugin-visualizer";
 
-const inputs = [{ format: "esm" }, { format: "cjs" }, { format: "umd" }];
+const inputs = [{ format: "es" }, { format: "cjs" }, { format: "umd" }];
+
+const outputFileExt = (format) => {
+  switch (format) {
+    case "es":
+      return "mjs";
+
+    case "cjs":
+      return "cjs";
+
+    default:
+      return "js";
+  }
+};
 
 const output = inputs.map(({ format }) => ({
-  file: `lib/${format}/meistericons-react.${format}.${
-    format === "esm" ? "m" : format === "cjs" ? "c" : ""
-  }js`,
-  sourcemap: true,
+  file: `lib/${format}/meistericons-react.${format}.${outputFileExt(format)}`,
+  // sourcemap: true,
 }));
 
 const minifyOutput = inputs.map(({ format }) => ({
   name: pkg.name,
-  file: `lib/${format}/meistericons-react.${format}.min.${
-    format == "esm" ? "m" : format === "cjs" ? "c" : ""
-  }js`,
-  plugins: [minify()],
-  sourcemap: true,
+  file: `lib/${format}/meistericons-react.${format}.min.${outputFileExt(
+    format
+  )}`,
+  plugins: [minify(), terser()],
+  // sourcemap: true,
 }));
 
 export default {
@@ -27,9 +39,9 @@ export default {
   output: [...output, ...minifyOutput],
   external: ["react"],
   plugins: [
-    esbuild(),
-    sourcemaps(),
-    resolve({ preferBuiltins: true }),
+    esbuild({ optimizeDeps: { include: ["react"] } }),
+    resolve({ preferBuiltins: true, browser: true }),
     commonjs(),
+    visualizer(),
   ],
 };
